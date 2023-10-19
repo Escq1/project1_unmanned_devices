c339c234115d4d5b33cca35864b725da9222ef15
Фоменко Елизавета Ивановна <elfomenko@sfedu.ru>
2023-10-19 15:20:37 +0300

    add new branch

--- Project_1.py
+++ Project_1.py
@@ -10,4 +10,20 @@ class ImageProcess:
         with Image.open(filename) as img:
             img.load()
 
+    def linear_operator(img_in, contrast, brightness):
+        r, g, b = img.split()
+        w,h = img_in.size
+        img_out = Image.new('RGB', (w,h))
+        for x in range(w):
+            for y in range(h):
+                pxl_r = r.getpixel((x,y))
+                result_pxl = int(contrast*original_pxl+brightness)
+                    if result_pxl < 0:
+                        result_pxl = 0
+                    if result_pxl > 255:
+                        result_pxl = 255
+                img_out.putpixel((x,y),result_pxl)
+    return(img_out)
+    
+
     
